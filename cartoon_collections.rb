4106ede66e37afd0d14dbb19a9441dad8a26f935
Daksha Patel <dakshapatel100@gmail.com>
2018-05-04 16:06:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,10 +3,9 @@ def roll_call_dwarves(dwarf_names)# code an argument here
 end
 
 def summon_captain_planet(planeteer_calls)
-  new_array = planeteer_calls
-  planeteer_calls.map!(&:upcase)
-  new_array.insert(i,!)
-  # Your code here
+
+  planeteer_calls.map {|call| call.capitalize + "!"}
+
 end
 
 def long_planeteer_calls# code an argument here
